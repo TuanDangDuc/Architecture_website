d16a77d684e94670e324c80e6963d834914837f2
Dang Duc Tuan <tdang9078@gmail.com>
2026-02-25 20:51:49 +0700

    init project

---
+++ src/main/java/com/Architecture_Website/Architecture_Website/ArchitectureWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.Architecture_Website.Architecture_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Architecture_Website/Architecture_Website/ArchitectureWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Architecture_Website.Architecture_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
